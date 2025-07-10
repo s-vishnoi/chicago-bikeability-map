43f032e9b83f739a85374a4cd016e875cb1f339b
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-07-10 14:19:16 -0500

    swag added

--- app.py
+++ app.py
@@ -246,4 +246,4 @@ def update_figure(clickData, dropdown_value):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, port=5003)
+    app.run(debug=True, use_reloader=False, port=5005)
